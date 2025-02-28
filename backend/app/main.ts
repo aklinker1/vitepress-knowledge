e11b8dacd3975ff92829fadb41de89f96c407389
Aaron <aaronklinker1@gmail.com>
2025-02-28 10:50:40 -0600

    Cleanup

--- backend/app/main.ts
+++ backend/app/main.ts
@@ -2,8 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createRouter, createWebHistory, RouterView } from "vue-router";
 
-const url = new URL(location.href);
-
 const router = createRouter({
   history: createWebHistory(),
   routes: [{ path: "/:pathMatch(.*)*", component: App }],
