346da3275f65a07711e1d05fa695a2aee370a4f3
Aaron <aaronklinker1@gmail.com>
2025-03-17 16:11:53 -0500

    feat: Add syntax highlighting to code blocks (#22)

---
+++ backend/app/composables/useMdToHtml.ts
@@ -0,0 +1,22 @@
+import { useEventListener, computedWithControl } from "@vueuse/core";
+import {
+  mdToHtml,
+  MARKDOWN_SYNTAX_HIGHLIGHTER_READY_EVENT,
+} from "../utils/md-to-html";
+import { computed, ref, toValue, type MaybeRefOrGetter } from "vue";
+import type { ComputedRef } from "vue";
+
+export default function (md: MaybeRefOrGetter<string>): ComputedRef<string> {
+  const trigger = ref(0);
+
+  const html = computed(() => {
+    // Access the trigger value so this value is recomputed when it changes.
+    void trigger.value;
+
+    return mdToHtml(toValue(md));
+  });
+  useEventListener(window, MARKDOWN_SYNTAX_HIGHLIGHTER_READY_EVENT, () => {
+    trigger.value++;
+  });
+  return html;
+}
--- backend/app/utils/md-to-html.ts
+++ backend/app/utils/md-to-html.ts
@@ -1,11 +1,56 @@
 import markdownit from "markdown-it";
 import type { RenderRule } from "markdown-it/lib/renderer.mjs";
+import { fromHighlighter } from "@shikijs/markdown-it/core";
+import { createHighlighterCore } from "shiki/core";
+import { createOnigurumaEngine } from "shiki/engine/oniguruma";
 
 const renderer = markdownit({
   typographer: true,
 })
   .use(linkBaseUrlPlugin)
   .use(linkTargetPlugin);
+
+export const MARKDOWN_SYNTAX_HIGHLIGHTER_READY_EVENT =
+  "markdown-syntax-highlighter-ready";
+
+// Load syntax highlighting
+(async () => {
+  const highlighter = await createHighlighterCore({
+    themes: [import("@shikijs/themes/github-dark")],
+    langs: [
+      import("@shikijs/langs/html"),
+      import("@shikijs/langs/css"),
+      import("@shikijs/langs/json"),
+      import("@shikijs/langs/javascript"),
+      import("@shikijs/langs/typescript"),
+      import("@shikijs/langs/jsx"),
+      import("@shikijs/langs/tsx"),
+      import("@shikijs/langs/vue"),
+      import("@shikijs/langs/svelte"),
+    ],
+    engine: createOnigurumaEngine(() => import("shiki/wasm")),
+  });
+  renderer.use(
+    // @ts-expect-error: highlighter type error, but it works
+    fromHighlighter(highlighter, {
+      theme: "github-dark",
+      transformers: [
+        {
+          // The theme background conflicts with the message backgrounds, so we
+          // remove the custom background color and use Tailwind's. Only the
+          // text is affected by the theme.
+          name: "vitepress-knowledge:pre-background",
+          pre(node) {
+            delete node.properties.style;
+          },
+        },
+      ],
+    }),
+  );
+  window.dispatchEvent(
+    new CustomEvent(MARKDOWN_SYNTAX_HIGHLIGHTER_READY_EVENT),
+  );
+})();
 
 export function mdToHtml(md: string): string {
   return renderer.render(md);
