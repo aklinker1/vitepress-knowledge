7f571b58e989b77a2a5b0858fc8a907d2d75078b
Aaron <aaronklinker1@gmail.com>
2025-03-17 15:22:01 -0500

    Add markdown-it

--- backend/app/utils/md-to-html.ts
+++ backend/app/utils/md-to-html.ts
@@ -1,23 +1,30 @@
-import { Converter, type ShowdownExtension } from "showdown";
+import markdownit from "markdown-it";
+import type { RenderRule } from "markdown-it/lib/renderer.mjs";
 
-const converter = new Converter({
-  extensions: [linkBaseUrl(), linkTarget()],
-});
+const renderer = markdownit({
+  typographer: true,
+})
+  .use(linkBaseUrlPlugin)
+  .use(linkTargetPlugin);
 
 export function mdToHtml(md: string): string {
-  return converter.makeHtml(md);
+  return renderer.render(md);
 }
 
 /**
  * Add a base URL to the docs if anchors are absolute paths.
  * "/" -> "https://wxt.dev/"
  */
-function linkBaseUrl(): ShowdownExtension {
-  return {
-    type: "output",
-    regex: /href="\//g,
-    replace: `href="${DOCS_URL}/`,
+function linkBaseUrlPlugin(md: markdownit): void {
+  const addBaseUrlRule: RenderRule = (tokens, idx, options, env, self) => {
+    const token = tokens[idx];
+    const hrefIndex = token.attrIndex("href");
+    if (hrefIndex >= 0 && token.attrs?.[hrefIndex]?.[1]?.startsWith("/")) {
+      token.attrs[hrefIndex][1] = DOCS_URL + token.attrs[hrefIndex][1];
+    }
+    return self.renderToken(tokens, idx, options);
   };
+  md.renderer.rules.link_open = addBaseUrlRule;
 }
 
 /**
@@ -25,10 +32,13 @@ function linkBaseUrl(): ShowdownExtension {
  * conversation in the open tab, which would otherwise be cleared when
  * changing URLs.
  */
-function linkTarget(): ShowdownExtension {
-  return {
-    type: "output",
-    regex: /<a /g,
-    replace: `<a target="_blank" `,
+function linkTargetPlugin(md: markdownit): void {
+  const addTargetRule: RenderRule = (tokens, idx, options, env, self) => {
+    const token = tokens[idx];
+    if (token.attrIndex("target") < 0) {
+      token.attrPush(["target", "_blank"]);
+    }
+    return self.renderToken(tokens, idx, options);
   };
+  md.renderer.rules.link_open = addTargetRule;
 }
